f8a0f4b452310347c6db12817147f2823a71aece
p0six-00 <58270399+p0six-00@users.noreply.github.com>
2024-12-20 15:06:14 +0100

    ok

--- PlasmaTorch.py
+++ PlasmaTorch.py
@@ -1,1 +1,10 @@
-import deposition
+import torch
+import matplotlib.pyplot as plt
+
+import sys
+import os
+# Aggiungi la directory della repository al percorso dei moduli
+repo_path = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(repo_path)
+
+from deposition import *
